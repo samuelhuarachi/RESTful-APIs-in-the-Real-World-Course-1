375f856efcc08401ffc1cba927b403a839b16b93
Samuel Gomes <samuelgomesdev@bitbucket.org>
2018-03-08 15:09:16 -0300

    POST: Creation, Location Header and 201

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -3,6 +3,7 @@
 namespace KnpU\CodeBattle\Controller\Api;
 
 use KnpU\CodeBattle\Controller\BaseController;
+use KnpU\CodeBattle\Model\Programmer;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,9 +17,22 @@ class ProgrammerController extends BaseController
         $controllers->post('/api/programmers', array($this, 'newAction'));
     }
 
-    public function newAction()
+    public function newAction(Request $request)
     {
-        return "Let's Battle!";
+        $data = json_decode($request->getContent(), true);
+
+        $programmer = new Programmer();
+        $programmer->nickname = $data["nickname"];
+        $programmer->avatarNumber = $data["avatarNumber"];
+        $programmer->tagLine = $data["tagLine"];
+        $programmer->userId = $this->findUserByUsername("weaverryan")->id;
+
+        $this->save($programmer);
+
+        $response = new Response("It worked! Trust me, I\'m an API", 201);
+        $response->headers->set("Location", "/programmers/not/fake...");
+
+        return $response;
     }
 
 }
